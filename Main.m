6caff3ec88c21a0251dd4bd4f489812d646753bf
Cannonbait <ivar_busig@hotmail.com>
2016-11-22 15:42:13 +0100

    Basic structure

---
+++ Main.m
@@ -0,0 +1,31 @@
+NUMBER_WHALES = 100;
+NUMBER_KRILLS = 1000;
+AREA_SIZE = 100;
+
+WHALE_MOVEMENT_RATE = 5;
+KRILL_MOVEMENT_RATE = 2;
+
+krillPopulation = InitializeKrill(NUMBER_KRILLS, AREA_SIZE);
+whalePopulation = InitializeWhales(NUMBER_WHALES, AREA_SIZE);
+
+TIMESTEPS = 100;
+
+statistics = zeros(2,TIMESTEPS);
+
+for iTimestep = 1:TIMESTEPS
+  % Movement
+  krillPopulation = MoveKrill(krillPopulation, KRILL_MOVEMENT_RATE);
+  whalePopulation = MoveWhales(whalePopulation, krillPopulation, WHALE_MOVEMENT_RATE);
+  
+  %Predation
+  [krillPopulation,whalePopulation] = Predation(krillPopulation, whalePopulation);
+  
+  %Breeding
+  whalePopulation = WhaleBreeding(whalePopulation);
+  krillPopulation = KrillBreeding(krillPopulation);
+  
+  %Starvation
+  whalePopulation = WhaleStarvation(whalePopulation);
+  
+end
+  
