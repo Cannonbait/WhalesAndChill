0a8a3fac218b25ead8061e4f3c00ae7b6c2f0f25
sidneni <siddhartha.kasaraneni@gmail.com>
2016-11-28 00:03:52 +0100

    new whale breeding

--- WhaleBreeding.m
+++ WhaleBreeding.m
@@ -1,3 +1,11 @@
-function whalePopulation = WhaleBreeding(whalePopulation)
-
+function whalePopulation = WhaleBreeding(whalePopulation,Whale_breed_Score,GRID_SIZE)
+% add new whales
+ind = size(find(whalePopulation(:,3)>=Whale_breed_Score),1);
+new = randi([1 GRID_SIZE],ind,2);
+new(:,3) = 1;
+whalePopulation = vertcat(whalePopulation,new);
+% remove score for the matured
+Matured = whalePopulation(:,3);
+Matured(Matured==Whale_breed_Score) = 1;
+whalePopulation(:,3) = Matured;
 end
