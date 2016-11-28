b790b3b893b0e514db02e4cf5e68796fba31ad9e
sidneni <siddhartha.kasaraneni@gmail.com>
2016-11-28 11:58:06 +0100

    modified main

--- Main.m
+++ Main.m
@@ -4,9 +4,10 @@ NUMBER_WHALES = 100;
 NUMBER_KRILLS = 1000;
 AREA_SIZE = 100;
 
-WHALE_MOVEMENT_RATE = 5;
-KRILL_MOVEMENT_RATE = 2;
+WHALE_MOVEMENT_RATE = 0.5;
+KRILL_MOVEMENT_RATE = 0.2;
 Whale_breed_Score = 50;
+New_Krills_Limit = 50;
 krillPopulation = InitializeKrill(NUMBER_KRILLS, AREA_SIZE);
 whalePopulation = InitializeWhales(NUMBER_WHALES, AREA_SIZE);
 
@@ -21,11 +22,11 @@ for iTimestep = 1:TIMESTEPS
   
   %Predation
   [krillPopulation,whalePopulation] = Predation(krillPopulation, whalePopulation,...
-  AREA_SIZE,NUMBER_KRILLS,NUMBER_WHALES);
+                                                                 AREA_SIZE);
   
   %Breeding
   whalePopulation = WhaleBreeding(whalePopulation,Whale_breed_Score,GRID_SIZE);
-  krillPopulation = KrillBreeding(krillPopulation);
+  krillPopulation = KrillBreeding(krillPopulation,New_Krills_Limit,GRID_SIZE);
   
   %Starvation
   whalePopulation = WhaleStarvation(whalePopulation,TIMESTEPS,t,Starvation_Value,Interval);
