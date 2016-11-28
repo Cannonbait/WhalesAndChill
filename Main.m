e60f34e70e7f91f026ed98ca4d147a5c6db1484d
sidneni <siddhartha.kasaraneni@gmail.com>
2016-11-28 10:09:24 +0100

    Whlae stavation +some changes in main

--- Main.m
+++ Main.m
@@ -1,17 +1,19 @@
+clc
+clear;close all;
 NUMBER_WHALES = 100;
 NUMBER_KRILLS = 1000;
 AREA_SIZE = 100;
 
 WHALE_MOVEMENT_RATE = 5;
 KRILL_MOVEMENT_RATE = 2;
-
+Whale_breed_Score = 50;
 krillPopulation = InitializeKrill(NUMBER_KRILLS, AREA_SIZE);
 whalePopulation = InitializeWhales(NUMBER_WHALES, AREA_SIZE);
 
 TIMESTEPS = 100;
 
 statistics = zeros(2,TIMESTEPS);
-
+t = 1;
 for iTimestep = 1:TIMESTEPS
   % Movement
   krillPopulation = MoveKrill(krillPopulation, KRILL_MOVEMENT_RATE,AREA_SIZE);
@@ -22,11 +24,12 @@ for iTimestep = 1:TIMESTEPS
   AREA_SIZE,NUMBER_KRILLS,NUMBER_WHALES);
   
   %Breeding
-  whalePopulation = WhaleBreeding(whalePopulation);
+  whalePopulation = WhaleBreeding(whalePopulation,Whale_breed_Score,GRID_SIZE);
   krillPopulation = KrillBreeding(krillPopulation);
   
   %Starvation
-  whalePopulation = WhaleStarvation(whalePopulation);
+  whalePopulation = WhaleStarvation(whalePopulation,TIMESTEPS,t,Starvation_Value,Interval);
   
+  t = t + 1;
 end
   
--- WhaleStarvation.m
+++ WhaleStarvation.m
@@ -1,3 +1,12 @@
-function whalePopulation = WhaleStarvation(whalePopulation)
+function whalePopulation = WhaleStarvation(whalePopulation,TIMESTEPS,t,Starvation_Value,Interval)
+%Check for whales with low score at an interval of 50 timespteps
+%if the score is <= starvation value the whale is removed
+
+Int = Interval : Interval : TIMESTEPS;
+if ismember(t,Int)
+    Remove = whalePopulation(:,3)>=Starvation_Value;
+    [x] = find(whalePopulation(:,1).*Remove);
+    whalePopulation = whalePopulation(x,:);
+end
 
 end
