b9b55af8fd27014e0c63e62af2dcc3bded328b97
sidneni <siddhartha.kasaraneni@gmail.com>
2016-11-28 11:58:18 +0100

    modified predation

--- Predation.m
+++ Predation.m
@@ -1,22 +1,22 @@
-function [krillPopulation,whalePopulation] = Predation(krillPopulation, whalePopulation,AREA_SIZE,NUMBER_KRILLS,NUMBER_WHALES)
+function [krillPopulation,whalePopulation] = Predation(krillPopulation, whalePopulation,AREA_SIZE)
 
 WhalePos = zeros(AREA_SIZE);
-for i = 1:NUMBER_WHALES
+for i = 1:size(whalePopulation,1)
 WhalePos(whalePopulation(i,1),whalePopulation(i,2)) = 1;
 end
 
 KrillPos = zeros(AREA_SIZE);
-for i = 1:NUMBER_KRILLS
+for i = 1:size(krillPopulation,1)
 KrillPos(krillPopulation(i,1),krillPopulation(i,2)) = 1;
 end
-%remove Krill
+%Remove Krill
 [Pos(:,1),Pos(:,2)] = find(KrillPos & WhalePos == 1);
 for i = 1:length(Pos)
 indx1(:,i) = (Pos(i,1)==krillPopulation(:,1))&(Pos(i,2)==krillPopulation(:,2));
 end
 [x] = find(krillPopulation(:,1).*(sum(indx1,2)==0));
 krillPopulation = krillPopulation(x,:);
-%whale score
+%Whale score
 for i = 1:length(Pos)
 indx2(:,i) = (Pos(i,1)==whalePopulation(:,1))&(Pos(i,2)==whalePopulation(:,2));
 end
