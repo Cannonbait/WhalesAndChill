49c4c959063640ec67ea9b5253a4246bc1272c4f
Cannonbait <ivar_busig@hotmail.com>
2016-11-30 14:56:40 +0100

    Refactored Predation for matrix

--- Predation.m
+++ Predation.m
@@ -1,25 +1,10 @@
-function [krillPopulation,whalePopulation] = Predation(krillPopulation, whalePopulation,AREA_SIZE)
-  WhalePos = zeros(AREA_SIZE);
-  for i = 1:size(whalePopulation,1)
-    WhalePos(whalePopulation(i,1),whalePopulation(i,2)) = 1;
-  end
-
-  KrillPos = zeros(AREA_SIZE);
-  for i = 1:size(krillPopulation,1)
-    KrillPos(krillPopulation(i,1),krillPopulation(i,2)) = 1;
-  end
-  %Remove Krill
-  [Pos(:,1),Pos(:,2)] = find(KrillPos & WhalePos == 1);
-  for i = 1:size(Pos,1)
-    indx1(:,i) = (Pos(i,1)==krillPopulation(:,1))&(Pos(i,2)==krillPopulation(:,2));
-  end
-  if size(Pos,1)>0
-    [x] = find(krillPopulation(:,1).*(sum(indx1,2)==0));
-    krillPopulation = krillPopulation(x,:);
-    %Whale score
-    for i = 1:size(Pos,1)
-      indx2(:,i) = (Pos(i,1)==whalePopulation(:,1))&(Pos(i,2)==whalePopulation(:,2));
+function [krillPopulation,whalePopulation] = Predation(krillPopulation, whalePopulation, fullnessIncreaseWhales)
+  whales = find(whalePopulation > 0);
+  
+  for i = 1:size(whales)
+    if krillPopulation(whales(i)) > 0
+      krillPopulation(whales(i)) = 0;
+      whalePopulation(whales(i)) = whalePopulation(whales(i)) + fullnessIncreaseWhales;
     end
-    whalePopulation(:,3) = whalePopulation(:,3) + (sum(indx2,2)==1);
   end
 end
