40c24c3e682f0f671ff34af3b013f44ab75f0713
Cannonbait <ivar_busig@hotmail.com>
2016-11-30 13:20:06 +0100

    Replace for loop with arrayfun

--- WhaleStarvation.m
+++ WhaleStarvation.m
@@ -1,8 +1,5 @@
 function whalePopulation = WhaleStarvation(whalePopulation, starvationRate, minFoodSurvival)
-  newWhalePopulation = whalePopulation;
-  for iWhale = 1:size(whalePopulation,1)
-    newWhalePopulation(iWhale,3) = whalePopulation(iWhale,3)-starvationRate;
-  end
+  whalePopulation(:,3) = arrayfun(@(x) x-1, whalePopulation(:,3));
   Remove = whalePopulation(:,3)>=minFoodSurvival;
   [x] = find(whalePopulation(:,1).*Remove);
   whalePopulation = whalePopulation(x,:);
