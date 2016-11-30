1db6859876727cb19421cb9d9d65062d66837461
Carl Fredriksson <c@msp.se>
2016-11-30 11:28:37 +0100

    Minor formatting.

--- MoveKrill.m
+++ MoveKrill.m
@@ -4,9 +4,10 @@ VonNeuman = [1 -1 0 0; 0 0 1 -1]; % VonNeuman Neighbourhood
 XPos = krillPopulation(:,1);
 YPos = krillPopulation(:,2);
 for i = 1:n
-    for t= 1:4
-        i2 = XPos(i)+VonNeuman(1,t); j2= YPos(i)+VonNeuman(2,t);
-        if i2>0 && i2 <AREA_SIZE+1 && j2>0 && j2 <AREA_SIZE+1 && rand < KRILL_MOVEMENT_RATE
+    for t = 1:4
+        i2 = XPos(i)+VonNeuman(1,t);
+        j2 = YPos(i)+VonNeuman(2,t);
+        if i2 > 0 && i2 < AREA_SIZE+1 && j2 > 0 && j2 < AREA_SIZE+1 && rand < KRILL_MOVEMENT_RATE
             krillPopulation(i,1) = krillPopulation(i,1)+VonNeuman(1,t);
             krillPopulation(i,2) = krillPopulation(i,2)+VonNeuman(2,t);
         end
--- MoveWhales.m
+++ MoveWhales.m
@@ -5,8 +5,9 @@ XPos = whalePopulation(:,1);
 YPos = whalePopulation(:,2);
 for i = 1:n
     for t= 1:4
-        i2 = XPos(i)+VonNeuman(1,t); j2= YPos(i)+VonNeuman(2,t);
-        if i2>0 && i2 <AREA_SIZE+1 && j2>0 && j2 <AREA_SIZE+1 && rand < WHALE_MOVEMENT_RATE
+        i2 = XPos(i)+VonNeuman(1,t);
+        j2 = YPos(i)+VonNeuman(2,t);
+        if i2 > 0 && i2 < AREA_SIZE+1 && j2 > 0 && j2 < AREA_SIZE+1 && rand < WHALE_MOVEMENT_RATE
             whalePopulation(i,1) = whalePopulation(i,1)+VonNeuman(1,t);
             whalePopulation(i,2) = whalePopulation(i,2)+VonNeuman(2,t);
         end
