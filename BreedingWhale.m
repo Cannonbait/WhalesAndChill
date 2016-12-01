81f1424dfdfc07bfbba7697ed3ff3da36c34bc7e
Cannonbait <ivar_busig@hotmail.com>
2016-12-01 15:44:03 +0100

    Timespan for breeding of whales

--- BreedingWhale.m
+++ BreedingWhale.m
@@ -1,13 +1,15 @@
-function whalePopulation = BreedingWhale(whalePopulation, whaleBreedRequirement,breedFullnessReduction, initialFullness)
-  pregnantWhales = find(whalePopulation > whaleBreedRequirement);
-  numPregnantWhales = length(pregnantWhales);
-  emptySpaces = find(whalePopulation == 0);
-  numEmptySpaces = length(emptySpaces);
-  numNewWhales = min(numPregnantWhales, numEmptySpaces);
-  randLinearIndices = randperm(numEmptySpaces, numNewWhales);
-  for i = 1:numNewWhales
-    whalePopulation(pregnantWhales(i)) = whalePopulation(pregnantWhales(i))*breedFullnessReduction;
-    position = emptySpaces(randLinearIndices(i));
-    whalePopulation(position) = initialFullness;
-  end  
+function whalePopulation = BreedingWhale(whalePopulation, whaleBreedRequirement,breedFullnessReduction, initialFullness, iTimestep, breedingCycle)
+  if (mod(iTimestep, breedingCycle) == 0)
+    pregnantWhales = find(whalePopulation > whaleBreedRequirement);
+    numPregnantWhales = length(pregnantWhales);
+    emptySpaces = find(whalePopulation == 0);
+    numEmptySpaces = length(emptySpaces);
+    numNewWhales = min(numPregnantWhales, numEmptySpaces);
+    randLinearIndices = randperm(numEmptySpaces, numNewWhales);
+    for i = 1:numNewWhales
+      whalePopulation(pregnantWhales(i)) = whalePopulation(pregnantWhales(i))-breedFullnessReduction;
+      position = emptySpaces(randLinearIndices(i));
+      whalePopulation(position) = initialFullness;
+    end  
+  end
 end
