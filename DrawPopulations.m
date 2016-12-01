ed9bac7d83452a4651785fc0fb033db697db7e7f
Carl Fredriksson <c@msp.se>
2016-12-01 11:27:37 +0100

    minor formatting.

--- DrawPopulations.m
+++ DrawPopulations.m
@@ -1,11 +1,7 @@
 function DrawPopulations(whalePopulation, krillPopulation) 
-  
   lattice = sign(whalePopulation)*2 + krillPopulation;
   
-  
   % Plot
-%   figure(1);
-%   clf;
   imagesc(lattice);
   colorMap = [1 1 1; 1 0 0; 0 0 1];
   colormap(colorMap);
